59af492e6036af2595c5679ef2cc5e35c01d88cd
fhenri <fhenri@fhsolution.com>
2016-03-16 18:52:17 +0100

    add java module for app instance

--- puppet/script/install-puppet-modules-app.sh
+++ puppet/script/install-puppet-modules-app.sh
@@ -9,3 +9,7 @@ fi
 if [ ! -d /etc/puppet/modules/puppetlabs-apache ]; then
   puppet module install puppetlabs-apache --version 1.5.0
 fi
+
+if [ ! -d /etc/puppet/modules/puppetlabs-java ]; then
+  puppet module install puppetlabs-java --version 1.4.1
+fi
