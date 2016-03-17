8a36fe13c7b413696e53d6d2d631777ea5874f56
fhenri <fhenri@fhsolution.com>
2016-03-17 09:47:22 +0100

    fix script to make sure we differentiate initial run vs daily run

--- puppet/script/run-ariba-app.sh
+++ puppet/script/run-ariba-app.sh
@@ -1,3 +1,8 @@
-git clone git@bitbucket.org:fhenri/powersource.git /home/ariba/pws_sources
-cd /home/ariba/pws_sources && git checkout aws_dev
-cd /home/ariba/pws_sources && ant amazon
+if [ -d "/home/ariba/pws_sources"];then
+    cd /home/ariba/pws_sources && git pull
+    cd /home/ariba/pws_sources && ant amazon
+else
+    git clone git@bitbucket.org:fhenri/powersource.git /home/ariba/pws_sources
+    cd /home/ariba/pws_sources && git checkout aws_dev
+fi
+
--- puppet/script/run-test.sh
+++ puppet/script/run-test.sh
@@ -1,9 +1,14 @@
-cd /home/ariba && nohup java -jar selenium-server-standalone-2.50.1.jar -role hub &> /home/ariba/nohup.grid.out&
+if [ -d "/home/ariba/pws_test"];then
+    cd /home/ariba && nohup java -jar selenium-server-standalone-2.50.1.jar -role hub &> /home/ariba/nohup.grid.out&
+    
+    cd /home/ariba/pws_test && git pull
+    cd /home/ariba/pws_test && gradle build
 
-git clone git@bitbucket.org:fhenri/powersource.git /home/ariba/pws_test
-cd /home/ariba/pws_test && git checkout TST_Serenity
-cd /home/ariba/pws_test && gradle build
-
-cd /home/ariba/pws_test && gradle aggregate
-DATE=`date +%Y-%m-%d`
-cp -R /home/ariba/pws_test/target/site/serenity /vagrant/$DATE
+    cd /home/ariba/pws_test && gradle aggregate
+    DATE=`date +%Y-%m-%d`
+    cp -R /home/ariba/pws_test/target/site/serenity /vagrant/$DATE
+else
+    git clone git@bitbucket.org:fhenri/powersource.git /home/ariba/pws_test
+    cd /home/ariba/pws_test && git checkout TST_Serenity
+    cd /home/ariba/pws_test && gradle assemble
+fi
